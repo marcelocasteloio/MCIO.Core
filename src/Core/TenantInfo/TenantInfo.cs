2f87f74dce2518b61705e3835174295906825a0e
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2024-02-02 01:07:21 -0300

    feat: add tenant info

---
+++ src/Core/TenantInfo/TenantInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MCIO.Core.TenantInfo
+{
+    public readonly struct TenantInfo
+    {
+        // Properties
+        public Guid Code { get; }
+        public bool IsValid { get; }
+
+        // Constructors
+        private TenantInfo(Guid code)
+        {
+            Code = code;
+            IsValid = true;
+        }
+
+        // Operators
+        public static implicit operator Guid(TenantInfo value) => value.Code;
+        public static implicit operator TenantInfo(Guid value) => new TenantInfo(value);
+
+        // Builders
+        public static TenantInfo FromExistingCode(Guid code) => new TenantInfo(code);
+    }
+}
---
+++ tst/UnitTests/TenantInfoTests/TenantInfoTest.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+
+namespace MCIO.Core.UnitTests.TenantInfoTests;
+
+public class TenantInfoTest
+{
+    [Fact]
+    public void TenantInfo_Should_Created_From_Existing_Code()
+    {
+        // Arrange
+        var existingCode = Guid.NewGuid();
+
+        // Act
+        var tenantInfoA = TenantInfo.TenantInfo.FromExistingCode(existingCode);
+        var tenantInfoB = (TenantInfo.TenantInfo) existingCode;
+        var tenantCodeA = tenantInfoA.Code;
+        var tenantCodeB = (Guid) tenantInfoB;
+
+        // Assert
+        tenantInfoA.IsValid.Should().BeTrue();
+        tenantInfoA.Code.Should().Be(existingCode);
+        tenantCodeA.Should().Be(existingCode);
+
+        tenantInfoB.IsValid.Should().BeTrue();
+        tenantInfoB.Code.Should().Be(existingCode);
+        tenantCodeB.Should().Be(existingCode);
+    }
+    [Fact]
+    public void TenantInfo_Should_Implicited_Converted_From()
+    {
+        // Arrange
+        var existingCode = Guid.NewGuid();
+
+        // Act
+        var tenantInfo = TenantInfo.TenantInfo.FromExistingCode(existingCode);
+
+        // Assert
+        tenantInfo.IsValid.Should().BeTrue();
+        tenantInfo.Code.Should().Be(existingCode);
+    }
+}
