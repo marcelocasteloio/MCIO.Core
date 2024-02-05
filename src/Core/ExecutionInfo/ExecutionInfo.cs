775bbaa271ae73dafa227b03b1f5ffb4d8856faa
MarceloCas <117119821+MarceloCas@users.noreply.github.com>
2024-02-04 22:07:44 -0300

    feat: add execution info (#2)

---
+++ src/Core/ExecutionInfo/ExecutionInfo.cs
@@ -0,0 +1,102 @@
+﻿using MCIO.OutputEnvelop;
+using MCIO.OutputEnvelop.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MCIO.Core.ExecutionInfo
+{
+    public readonly struct ExecutionInfo
+    {
+        // Constants
+        public const int MAX_MESSAGE_COUNT = 4;
+
+        public const string CORRELATION_ID_SHOULD_REQUIRED_MESSAGE_CODE = "ExecutionInfo.CorrelationId.Should.Required";
+        public const string CORRELATION_ID_SHOULD_REQUIRED_MESSAGE_DESCRIPTION = "CorrelationId should required";
+
+        public const string TENANT_INFO_SHOULD_VALID_MESSAGE_CODE = "ExecutionInfo.TenantInfo.Should.Valid";
+        public const string TENANT_INFO_SHOULD_VALID_MESSAGE_DESCRIPTION = "TenantInfo should valid";
+
+        public const string EXECUTION_USER_SHOULD_REQUIRED_MESSAGE_CODE = "ExecutionInfo.ExecutionUser.Should.Required";
+        public const string EXECUTION_USER_SHOULD_REQUIRED_MESSAGE_DESCRIPTION = "ExecutionUser should required";
+
+        public const string ORIGIN_SHOULD_REQUIRED_MESSAGE_CODE = "ExecutionInfo.Origin.Should.Required";
+        public const string ORIGIN_SHOULD_REQUIRED_MESSAGE_DESCRIPTION = "Origin should required";
+
+        // Properties
+        public Guid CorrelationId { get; }
+        public TenantInfo.TenantInfo TenantInfo { get; }
+        public string ExecutionUser { get; }
+        public string Origin { get; }
+        public bool IsValid { get; }
+
+        // Constructors
+        private ExecutionInfo(
+            Guid correlationId,
+            TenantInfo.TenantInfo tenantInfo,
+            string executionUser,
+            string origin
+        )
+        {
+            CorrelationId = correlationId;
+            TenantInfo = tenantInfo;
+            ExecutionUser = executionUser;
+            Origin = origin;
+            IsValid = true;
+        }
+
+        // Builders
+        public static OutputEnvelop<ExecutionInfo?> Create(
+            Guid correlationId,
+            TenantInfo.TenantInfo tenantInfo,
+            string executionUser,
+            string origin
+        )
+        {
+            // Validation
+            var outputMessageCollection = new List<OutputMessage>(capacity: MAX_MESSAGE_COUNT);
+
+            if (correlationId == Guid.Empty)
+                outputMessageCollection.Add(
+                    OutputMessage.CreateError(
+                        CORRELATION_ID_SHOULD_REQUIRED_MESSAGE_CODE, 
+                        CORRELATION_ID_SHOULD_REQUIRED_MESSAGE_DESCRIPTION
+                    )
+                );
+
+            if (!tenantInfo.IsValid)
+                outputMessageCollection.Add(
+                    OutputMessage.CreateError(
+                        TENANT_INFO_SHOULD_VALID_MESSAGE_CODE,
+                        TENANT_INFO_SHOULD_VALID_MESSAGE_DESCRIPTION
+                    )
+                );
+
+            if (string.IsNullOrWhiteSpace(executionUser))
+                outputMessageCollection.Add(
+                    OutputMessage.CreateError(
+                        EXECUTION_USER_SHOULD_REQUIRED_MESSAGE_CODE,
+                        EXECUTION_USER_SHOULD_REQUIRED_MESSAGE_DESCRIPTION
+                    )
+                );
+
+            if (string.IsNullOrWhiteSpace(origin))
+                outputMessageCollection.Add(
+                    OutputMessage.CreateError(
+                        ORIGIN_SHOULD_REQUIRED_MESSAGE_CODE,
+                        ORIGIN_SHOULD_REQUIRED_MESSAGE_DESCRIPTION
+                    )
+                );
+
+            // Return
+            return outputMessageCollection.Count == 0
+                ? OutputEnvelop<ExecutionInfo?>.CreateSuccess(
+                    output: new ExecutionInfo(correlationId, tenantInfo, executionUser, origin)
+                )
+                : OutputEnvelop<ExecutionInfo?>.CreateError(
+                    output: null,
+                    outputMessageCollection: outputMessageCollection.ToArray(),
+                    exceptionCollection: null
+                );
+        }
+    }
+}
---
+++ tst/UnitTests/ExecutionInfoTests/ExecutionInfoTest.cs
@@ -0,0 +1,118 @@
+﻿using FluentAssertions;
+using MCIO.OutputEnvelop.Enums;
+
+namespace MCIO.Core.UnitTests.ExecutionInfoTests;
+
+public class ExecutionInfoTest
+{
+    [Fact]
+    public void ExecutionInfo_Should_Created()
+    {
+        // Arrange
+        var correlationId = Guid.NewGuid();
+        var tenantInfo = TenantInfo.TenantInfo.FromExistingCode(Guid.NewGuid()).Output!.Value;
+        var executionUser = Guid.NewGuid().ToString();
+        var origin = Guid.NewGuid().ToString();
+
+        // Act
+        var executionInfo = ExecutionInfo.ExecutionInfo.Create(
+            correlationId,
+            tenantInfo,
+            executionUser,
+            origin
+        ).Output!.Value;
+
+        // Assert
+        executionInfo.IsValid.Should().BeTrue();
+        executionInfo.CorrelationId.Should().Be(correlationId);
+        executionInfo.TenantInfo.Should().Be(tenantInfo);
+        executionInfo.ExecutionUser.Should().Be(executionUser);
+        executionInfo.Origin.Should().Be(origin);
+    }
+
+    [Fact]
+    public void ExecutionInfo_Should_Invalid_If_Created_From_Default()
+    {
+        // Arrange
+        var expectedIsValid = false;
+        var correlationId = Guid.Empty;
+        var tenantInfo = default(TenantInfo.TenantInfo);
+        var executionUser = default(string);
+        var origin = default(string);
+
+        // Arrange and Act
+        var executionInfo = default(ExecutionInfo.ExecutionInfo);
+
+        // Assert
+        executionInfo.IsValid.Should().Be(expectedIsValid);
+        executionInfo.CorrelationId.Should().Be(correlationId);
+        executionInfo.TenantInfo.Should().Be(tenantInfo);
+        executionInfo.ExecutionUser.Should().Be(executionUser);
+        executionInfo.Origin.Should().Be(origin);
+    }
+
+    [Fact]
+    public void ExecutionInfo_Should_Not_Created()
+    {
+        // Arrange
+        var correlationId = Guid.Empty;
+        var tenantInfo = default(TenantInfo.TenantInfo);
+        var executionUser = default(string);
+        var origin = default(string);
+
+        // Act
+        var executionInfoOutput = ExecutionInfo.ExecutionInfo.Create(
+            correlationId,
+            tenantInfo,
+            executionUser,
+            origin
+        );
+
+        // Assert
+        executionInfoOutput.IsSuccess.Should().BeFalse();
+        executionInfoOutput.HasOutput.Should().BeFalse();
+        executionInfoOutput.Output.Should().BeNull();
+
+        executionInfoOutput.OutputMessageCollectionCount
+            .Should()
+            .Be(
+                ExecutionInfo.ExecutionInfo.MAX_MESSAGE_COUNT
+            );
+
+        executionInfoOutput.OutputMessageCollection
+            .Any(q => 
+                q.Type == OutputMessageType.Error
+                && q.Code == ExecutionInfo.ExecutionInfo.CORRELATION_ID_SHOULD_REQUIRED_MESSAGE_CODE
+                && q.Description == ExecutionInfo.ExecutionInfo.CORRELATION_ID_SHOULD_REQUIRED_MESSAGE_DESCRIPTION
+            )
+            .Should()
+            .BeTrue();
+
+        executionInfoOutput.OutputMessageCollection
+            .Any(q =>
+                q.Type == OutputMessageType.Error
+                && q.Code == ExecutionInfo.ExecutionInfo.TENANT_INFO_SHOULD_VALID_MESSAGE_CODE
+                && q.Description == ExecutionInfo.ExecutionInfo.TENANT_INFO_SHOULD_VALID_MESSAGE_DESCRIPTION
+            )
+            .Should()
+            .BeTrue();
+
+        executionInfoOutput.OutputMessageCollection
+            .Any(q =>
+                q.Type == OutputMessageType.Error
+                && q.Code == ExecutionInfo.ExecutionInfo.EXECUTION_USER_SHOULD_REQUIRED_MESSAGE_CODE
+                && q.Description == ExecutionInfo.ExecutionInfo.EXECUTION_USER_SHOULD_REQUIRED_MESSAGE_DESCRIPTION
+            )
+            .Should()
+            .BeTrue();
+
+        executionInfoOutput.OutputMessageCollection
+            .Any(q =>
+                q.Type == OutputMessageType.Error
+                && q.Code == ExecutionInfo.ExecutionInfo.ORIGIN_SHOULD_REQUIRED_MESSAGE_CODE
+                && q.Description == ExecutionInfo.ExecutionInfo.ORIGIN_SHOULD_REQUIRED_MESSAGE_DESCRIPTION
+            )
+            .Should()
+            .BeTrue();
+    }
+}
